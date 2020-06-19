ce237598c2c9181a75973907adcc7f58b6546752
Guido Marsico <54811745+gmarsico93@users.noreply.github.com>
2020-06-19 15:10:16 -0300

    Update SaludoService.java

--- src/main/java/com/example/cloud/build/test/service/SaludoService.java
+++ src/main/java/com/example/cloud/build/test/service/SaludoService.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class SaludoService {
-
+    /***Comentario ***/
     public String saludar(String nombre){
         return "Chau "+nombre+"!!!" ;
     }
